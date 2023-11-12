3ff3f3af4519c0140b6cdb5c8a9a58942c7c95b4
Yordalina Metodieva <yordalina.metodieva@gmail.com>
2023-11-12 14:23:43 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+
+
+let portfolioButton = document.querySelector('.portfolioButton')
+let helloButton = document.querySelector('.helloButton')
+let buttonBla = document.getElementById('bla')
+
+console.log('ha')
+console.log(portfolioButton)
+console.log(buttonBla)
+
+
+// buttonBla.addEventListener('click', function () {
+//     console.log('natiska se bla butona')
+// })
+
+portfolioButton.addEventListener('click', function () {
+    console.log('natiska se portfolio butona')
+})
+
+helloButton.addEventListener('click', function () {
+    console.log('natiska se hello butona')
+})
+
+
+// function portfolioScreenActive() {
+//     console.log('vliza')
+//     document.querySelector('.main').innerHTML = portfolioScreen
+
+// }
+
